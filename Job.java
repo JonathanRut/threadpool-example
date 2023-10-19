b760720a6cb573c56d9d1202bb121dfdb5923472
JonathanRut <jmrutland@gmail.com>
2023-10-19 14:20:48 +0100

    added doc comments to Job class

--- Job.java
+++ Job.java
@@ -1,14 +1,14 @@
-public class Job implements Runnable{
+public class Job implements Runnable {
   private int jobNumber;
 
-  Job (int jobNumber){
-    this.jobNumber = jobNumber
+  Job(int jobNumber) {
+    this.jobNumber = jobNumber;
   }
 
-  public void run(){
+  public void run() {
     // Undertake required work, here we will emulate it by sleeping for a period
     System.out.println("Job: " + jobNumber + " is being processed by thread : " + Thread.currentThread().getName());
-    try{
+    try {
       Thread.sleep(1000);
     } catch (InterruptedException e) {
       // No catching as example should not experience interruptions
